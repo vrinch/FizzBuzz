2b562183ee8f5dc87744c50de3ee9aa91544d10c
Vincent Odukwe <51086859+vrinch@users.noreply.github.com>
2024-08-06 23:58:49 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+function run(N, M) {
+    const result = [];
+
+    for (let i = N; i <= M; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i.toString());
+        }
+    }
+
+    return result.join(',');
+}
+
+module.exports = { run };
